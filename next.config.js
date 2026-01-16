7e6216cd1d58cf7d7e7d3349b705292dc3d17067
contato305 <contato@gestsmart.com>
2026-01-16 16:51:15 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // permite next export
   output: 'export',
-  // evita otimizações de imagem que dependem do servidor
-  images: { unoptimized: true },
-  // gera /index.html, /about/index.html etc.
-  trailingSlash: true,
-};
+  images: {
+    unoptimized: true,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
